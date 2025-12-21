226fad5a71636ccfbf354a02cdc8d784ef22f2f9
vlex127 <iwunoayokunle@gmail.com>
2025-12-21 12:07:14 +0100

    ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from '@neondatabase/neon-js/auth';
+
 export const authClient = createAuthClient(
-    import.meta.env.NEON_AUTH_URL
-)
+  process.env.NEON_AUTH_URL || ''
+);
